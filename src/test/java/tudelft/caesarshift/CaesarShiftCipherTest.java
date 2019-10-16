95cb1324ee5dfa965c1d3d84de31cb8295d6b737
Przemyslaw Lib <przemyslaw.lib@hemmersbach.com>
2019-10-16 16:43:30 +0200

    Tests and fixes for Caesar shift cipher

--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,46 @@
 package tudelft.caesarshift;
 
-public class CaesarShiftCipherTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+public class CaesarShiftCipherTest {
+    private CaesarShiftCipher encoder;
+    final private int NEUTRAL_SHIFT = 0;
+
+    @BeforeEach
+    public void beforeEach() {
+        encoder = new CaesarShiftCipher();
+    }
+
+    @Test
+    public void spacesAreOK() {
+        String message = "a a";
+        Assertions.assertEquals(message, encoder.CaesarShiftCipher(message, NEUTRAL_SHIFT));
+    }
+
+    @Test
+    public void asIntoBs() {
+        Assertions.assertEquals("bb", encoder.CaesarShiftCipher("aa", 1));
+    }
+
+    @Test
+    public void asIntoZs() {
+        Assertions.assertEquals("zz", encoder.CaesarShiftCipher("aa", 25));
+    }
+
+    @Test
+    public void zeroTripIsNeutral() {
+        Assertions.assertEquals("aa", encoder.CaesarShiftCipher("aa", 0));
+    }
+
+    @Test
+    public void twentySixTripIsneutral() {
+        Assertions.assertEquals("aa", encoder.CaesarShiftCipher("aa", 26));
+    }
+
+    @Test
+    public void emptyStringIsStillEmpty() {
+        Assertions.assertEquals("", encoder.CaesarShiftCipher("", 3));
+    }
 }
